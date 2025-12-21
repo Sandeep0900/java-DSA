6aaeafd054f404f08825b5631f1412cfae729fb9
Sandeep0900 <sandeesharma09@gmail.com>
2025-12-21 13:51:13 +0530

    added the code of forloop to get the clearity of its functionality

--- MapLearning.java
+++ MapLearning.java
@@ -1,23 +1,9 @@
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Set;
-import java.util.HashSet;
-
 public class MapLearning{
     public static void main(String[] arr) {
-        Map<Integer, Boolean> NewMap = new HashMap<>();
-
-        System.out.println("HII");
-        NewMap.put(1,true);
-        NewMap.put(2,NewMap.containsKey(4));
-
-        Set<Integer> s = new HashSet<>();
-        s = NewMap.keySet();
-
-        for(int at:s ) {
-            if(NewMap.get(at) == true) {
-                System.out.println(at+" --->"+NewMap.get(at));
-            }
+        
+        for(int j=1 ; j > 0; j--) {
+            j = 0;
+            System.out.println(j);
         }
 
     }
--- stringToArray.java
+++ stringToArray.java
@@ -1,4 +1,4 @@
-import java.util.*;
+// import java.util.*;
 
 public class stringToArray {
     public static void main(String[] arr) {
